e1b7d88992c77231ba6d72964d37dbfcccc9e83f
leviano <leonivan3002@gmail.com>
2021-11-07 19:48:29 +0600

    Добавил отступы на виджетах

--- interface.py
+++ interface.py
@@ -1,35 +1,35 @@
 from tkinter import *
 root = Tk()
 root.title("Генератор паролей")
-root.resizable(width=False, height=False)   #запрет на изменение размера окна приложения
-w=(root.winfo_screenwidth())//2-225      #Размещение окна по центру экрана
-h=(root.winfo_screenheight())//2-162
-root.geometry("450x324+{}+{}".format(w,h))
+root.resizable(width=False, height=False)   # запрет на изменение размера окна приложения
+w = (root.winfo_screenwidth())//2-225      # Размещение окна по центру экрана
+h = (root.winfo_screenheight())//2-162
+root.geometry("450x324+{}+{}".format(w, h))
 
-#Виджеты приложения
-password_text = Text(root,height=14, width=30)
+# Виджеты приложения
+password_text = Text(root, height=14, width=30)
 
-generate_btn=Button(text='Сгенерировать')
-delete_btn=Button(text='Очистить')
-save_btn=Button(text='Сохранить')
+generate_btn = Button(text='Сгенерировать')
+delete_btn = Button(text='Очистить')
+save_btn = Button(text='Сохранить')
 
-num_lbl=Label(text='Количество паролей')
-num_ent=Entry(width=10, justify=CENTER)
+num_lbl = Label(text='Количество паролей')
+num_ent = Entry(width=10, justify=CENTER)
 
-len_lbl=Label(text='Длина паролей')
-len_ent=Entry(width=10, justify=CENTER)
+len_lbl = Label(text='Длина паролей')
+len_ent = Entry(width=10, justify=CENTER)
 
-#Расположение виджетов в окне приложения
-scrollbar=Scrollbar(root, command=password_text.yview)
-num_lbl.grid(row=0, column=0)
-num_ent.grid(row=0, column=1)
-len_lbl.grid(row=1, column=0)
-len_ent.grid(row=1, column=1)
-generate_btn.grid(row=3, column=0)
-delete_btn.grid(row=3, column=1)
-save_btn.grid(row=3, column=2)
-scrollbar.grid(row=4, column=3, sticky='nes')
-password_text.grid(row=4, column=0, sticky='nsew', columnspan=3)
+# Расположение виджетов в окне приложения
+scrollbar = Scrollbar(root, command=password_text.yview)
+num_lbl.grid(row=0, column=0, padx=30, pady=5, sticky='w')
+num_ent.grid(row=0, column=1, padx=30, pady=5, sticky='w')
+len_lbl.grid(row=1, column=0, padx=30, pady=5, sticky='w')
+len_ent.grid(row=1, column=1, padx=30, pady=5, sticky='w')
+generate_btn.grid(row=3, column=0, padx=30, pady=5, sticky='e')
+delete_btn.grid(row=3, column=1, padx=30, pady=5, sticky='w')
+save_btn.grid(row=3, column=2, padx=50, pady=5, sticky='w')
+scrollbar.grid(row=4, column=1, sticky='nes')
+password_text.grid(row=4, column=0, sticky='nsew', columnspan=2)
 password_text.configure(yscrollcommand=scrollbar.set)
 
 root.mainloop()
